6c63ed1b857e35450971d9356a18900edcbf6599
Dmitry Sandalov <dmitry@sandalov.org>
2014-01-14 19:27:08 +0400

    initial

---
+++ gen/org/sandalov/openweather/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.sandalov.openweather;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/sandalov/openweather/MainActivity.java
@@ -0,0 +1,209 @@
+package org.sandalov.openweather;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private TextView cityField;
+	private TextView dateField;
+	private TextView temperatureField;
+	private TextView humidityField;
+	private TextView pressureField;
+	private TextView windSpeedField;
+	private TextView conditionField;
+	private LocationManager locationManager;
+	private String provider;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		cityField = (TextView) findViewById(R.id.city);
+		dateField = (TextView) findViewById(R.id.date);
+		temperatureField = (TextView) findViewById(R.id.temperature);
+		humidityField = (TextView) findViewById(R.id.humidity);
+		pressureField = (TextView) findViewById(R.id.pressure);
+		windSpeedField = (TextView) findViewById(R.id.windSpeed);
+		conditionField = (TextView) findViewById(R.id.condition);
+
+		// Get the location manager
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Define the criteria how to select the location provider -> use
+		// default
+		Criteria criteria = new Criteria();
+		provider = locationManager.getBestProvider(criteria, false);
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		// Initialize the location fields
+		if (location != null) {
+			System.out.println("Provider " + provider + " has been selected.");
+			onLocationChanged(location);
+		} else {
+			cityField.setText("Location not available");
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_update:
+	            updateWeather();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+	private void updateWeather() {
+		Location location = locationManager.getLastKnownLocation(provider);
+		// Initialize the location fields
+		if (location != null) {
+			System.out.println("Provider " + provider + " has been selected.");
+			onLocationChanged(location);
+		} else {
+			cityField.setText("Location not available");
+		}
+
+		System.out.println("update!");
+		
+	}
+
+	public void onLocationChanged(Location location) {
+		String lat = String.valueOf(location.getLatitude());
+		String lng = String.valueOf(location.getLongitude());
+		GetJsonWeather weather = new GetJsonWeather();
+		weather.execute(new String[]{lat, lng});
+	}
+	
+	private class GetJsonWeather extends AsyncTask<String, Void, String> {
+
+		private String cityName;
+		private String cityCountry;
+		private String temperature;
+		private String humidity;
+		private String pressure;
+		private String windSpeed;
+		private String condition;
+		private String date;		
+		
+		@Override
+		protected String doInBackground(String... params) {
+			
+			// Get JSON
+			DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+			HttpGet httpget = new HttpGet("http://api.openweathermap.org/data/2.5/find?lat=" + params[0] +"&lon=" + params[1] + "&units=metric&type=accurate&cnt=1&lang=ru&mode=json");
+			// Depends on your web service
+			httpget.setHeader("Content-type", "application/json");
+
+			InputStream inputStream = null;
+			String result = null;
+			try {
+			    HttpResponse response = httpclient.execute(httpget);      
+			    HttpEntity entity = response.getEntity();
+
+			    inputStream = entity.getContent();
+			    // json is UTF-8 by default
+			    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+			    StringBuilder sb = new StringBuilder();
+
+			    String line = null;
+			    while ((line = reader.readLine()) != null)
+			    {
+			        sb.append(line + "\n");
+			    }
+			    result = sb.toString();
+			} catch (Exception e) {
+			    // Oops
+			}
+			finally {
+			    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+			}
+
+			// Parse JSON
+			try {
+				JSONObject rootObj = new JSONObject(result);
+				JSONArray list = rootObj.getJSONArray("list");
+				for (int i=0; i<list.length(); i++) {
+					JSONObject listObj = list.getJSONObject(i);
+					cityName = listObj.getString("name");
+					
+					JSONObject sysObj = listObj.getJSONObject("sys");
+					cityCountry = sysObj.getString("country");
+					
+					JSONObject mainObj = listObj.getJSONObject("main");
+					temperature = String.valueOf(mainObj.getInt("temp"));
+					humidity = String.valueOf(mainObj.getInt("humidity"));
+					pressure = String.valueOf(mainObj.getInt("pressure"));
+					
+					JSONObject windObj = listObj.getJSONObject("wind");
+					windSpeed = String.valueOf(windObj.getInt("speed"));
+					
+					JSONObject weatherObj = listObj.getJSONArray("weather").getJSONObject(0);
+					condition = weatherObj.getString("main");
+				}				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			
+	    	SimpleDateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm:ss", Locale.US);
+	    	date = df.format(Calendar.getInstance().getTime());
+			
+			return null;
+		}
+		
+	    @Override
+	    protected void onPostExecute(String result) {
+			dateField.setText(date);
+	    	cityField.setText(cityName + " (" + cityCountry + ")");
+	    	temperatureField.setText(temperature);
+	    	humidityField.setText(humidity);
+	    	pressureField.setText(pressure);
+	    	windSpeedField.setText(windSpeed);
+	    	conditionField.setText(condition);
+	    	
+	    	Context context = getApplicationContext();
+	    	CharSequence text = "Weather updated";
+	    	int duration = Toast.LENGTH_SHORT;
+
+	    	Toast toast = Toast.makeText(context, text, duration);
+	    	toast.show();
+	    }
+		
+	}
+
+}
